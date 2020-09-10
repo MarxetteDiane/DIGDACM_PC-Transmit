fb95ec3b3e7dff4211221f99e57196389847eca1
MarxetteDiane <60373649+MarxetteDiane@users.noreply.github.com>
2020-09-10 21:34:26 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
+//
+//  main.c
+//  DIGDACM
+//
+//  Created by Marxette Diane Cabato on 9/10/20.
+//  Copyright © 2020 Marxette Diane Cabato. All rights reserved.
+//
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include "parityCalculation.h"
-#define MAX 256
+#include "Header.h"
+#define MAX 256000
 
 char *readFile(char *fileName);
+unsigned long binaryToDecimal(char *binary, int length);
+void textToBinary(char *text, int textLength, char *binary, int binaryLength);
+void decimalToBinary(int decimal, char *octet);
 char *strrev(char *str);
 
 // INPUT: /Users/marxette/Desktop/DIGDACM/input.txt
@@ -13,10 +24,12 @@ char *strrev(char *str);
 int main(void) {
     FILE *fp1, *fp2;
     char inFile[MAX], outFile[MAX], output[MAX];
-    int i, j;
-    char * fullText = (char *) malloc(44), * string = (char *) malloc(44);
-    const char *arr[strlen(fullText)];                                       //blocks are stored here
-    char stream[1000]="";
+    //int i, j;
+    char * fullText = (char *) malloc(440); //* string = (char *) malloc(440);
+    //const char *arr[strlen(fullText)];                                       //blocks are stored here
+    //char stream[MAX]="";
+    char *binary;
+    int textLength, binaryLength;
     
     //get the input file name from the user
     printf("Enter your input file name: ");
@@ -46,9 +59,20 @@ int main(void) {
     
     // reads and gets data from input file
     fullText = readFile(inFile);
+    printf("\n%s\n",fullText);
+    /*--------------------------*/
+    textLength = sizeof(fullText);
+    binaryLength = textLength * 9;      // 8 binary digits + 1 space separator
+    binary = malloc(binaryLength + 1);  // + 1 null terminator
+    if(binary == NULL)
+        exit(1);
     
+    textToBinary(fullText, textLength, binary, binaryLength);
+    printf("Your binary encoding is:\n%s\n", binary);
+    
+    /*--------------------------*/
     // converts data to binary
-    for (i = 0; i < strlen(fullText); i++) {
+    /*for (i = 0; i < strlen(fullText); i++) {
         for (j = 0; j < 8; j++) {
             string[j] = (fullText[i] & (1 << j)) ? '1' : '0';
         }
@@ -58,10 +82,12 @@ int main(void) {
         arr[i] = string;                            // stores binary into an array
         
         strcat(stream, arr[i]);                     // concatenates elements of array to form stream of data
-    }
+    }*/
     
-    strcpy(output,parityCheck(stream));             // 2D Parity Calculation: displays stream with appended parity bits
+    strcpy(output,parityCheck(binary));             // 2D Parity Calculation: gets data stream with appended parity bits
     printf("\n%s\n",output);
+    
+    free(binary);
     
     // close files
     fclose(fp1);
@@ -90,6 +116,51 @@ char *readFile(char *fileName) {
 
     return code;
 }
+
+void textToBinary(char *text, int textLength, char *binary, int binaryLength)
+{
+    char *octet = malloc(9);
+    if(octet == NULL)
+        exit(1);
+    while(*text)
+    {
+        decimalToBinary(*text, octet);
+        while(*octet)
+            *binary++ = *octet++;
+        *binary++ = ' ';
+        ++text;
+        octet -= 8;
+    }
+    *binary = '\0';
+    binary -= binaryLength;
+    free(octet);
+}
+
+void decimalToBinary(int decimal, char *octet)
+{
+    octet += 8;
+    *octet = '\0';
+    if(decimal == 0)
+    {
+        octet -= 8;
+        octet = "00000000";
+    }
+    else
+    {
+        while(decimal > 0)
+        {
+            --octet;
+            *octet = decimal % 2 + '0';
+            decimal = decimal / 2;
+        }
+    }
+    while(strlen(octet) < 8)
+    {
+        --octet;
+        *octet = '0';
+    }
+}
+
 
 // reverses string
 char *strrev(char *str) {
